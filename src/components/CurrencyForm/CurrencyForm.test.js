6e82b61343f483cac53d40b1010b99beffbbdcb5
joannaberlin <joanna.kot.ok@gmail.com>
2022-12-15 09:17:40 +0100

    Change CurrencyForm to test

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -8,67 +8,67 @@ describe('Component CurrencyForm', () => {
 		render(<CurrencyForm action={() => {}} />);
 	});
 	it('should run action callback with proper data on form submit', () => {
-		const action = jest.fn();
+		// const action = jest.fn();
 
-		// render component
-		render(<CurrencyForm action={action} />);
+		// // render component
+		// render(<CurrencyForm action={action} />);
 
-		// find “convert” button
-		const submitButton = screen.getByText('Convert');
+		// // find “convert” button
+		// const submitButton = screen.getByText('Convert');
 
-		// find fields elems
-		const amountField = screen.getByTestId('amount');
-		const fromField = screen.getByTestId('from-select');
-		const toField = screen.getByTestId('to-select');
+		// // find fields elems
+		// const amountField = screen.getByTestId('amount');
+		// const fromField = screen.getByTestId('from-select');
+		// const toField = screen.getByTestId('to-select');
 
-		// set test values to fields
-		userEvent.type(amountField, '20');
-		userEvent.selectOptions(fromField, 'PLN');
-		userEvent.selectOptions(toField, 'USD');
+		// // set test values to fields
+		// userEvent.type(amountField, '20');
+		// userEvent.selectOptions(fromField, 'PLN');
+		// userEvent.selectOptions(toField, 'USD');
 
-		// simulate user click on "convert" button
-		userEvent.click(submitButton);
+		// // simulate user click on "convert" button
+		// userEvent.click(submitButton);
 
-		// check if action callback was called once and with proper argument
-		expect(action).toHaveBeenCalledTimes(1);
-		expect(action).toHaveBeenCalledWith({
-			amount: 20,
-			from: 'PLN',
-			to: 'USD',
-		});
+		// // check if action callback was called once and with proper argument
+		// expect(action).toHaveBeenCalledTimes(1);
+		// expect(action).toHaveBeenCalledWith({
+		// 	amount: 20,
+		// 	from: 'PLN',
+		// 	to: 'USD',
+		// });
 
-		// const testCases = [
-		// 	{ amount: '100', from: 'PLN', to: 'USD' },
-		// 	{ amount: '20', from: 'USD', to: 'PLN' },
-		// 	{ amount: '200', from: 'PLN', to: 'USD' },
-		// 	{ amount: '345', from: 'USD', to: 'PLN' },
-		// ];
+		const testCases = [
+			{ amount: '100', from: 'PLN', to: 'USD' },
+			{ amount: '20', from: 'USD', to: 'PLN' },
+			{ amount: '200', from: 'PLN', to: 'USD' },
+			{ amount: '345', from: 'USD', to: 'PLN' },
+		];
 
-		// for (const testObj of testCases) {
-		// 	const action = jest.fn();
+		for (const testObj of testCases) {
+			const action = jest.fn();
 
-		// 	render(<CurrencyForm action={action} />);
+			render(<CurrencyForm action={action} />);
 
-		// 	const submitButton = screen.getByText('Convert');
+			const submitButton = screen.getByText('Convert');
 
-		// 	userEvent.click(submitButton);
+			userEvent.click(submitButton);
 
-		// 	const amountField = screen.getByTestId('amount');
-		// 	const fromField = screen.getByTestId('from-select');
-		// 	const toField = screen.getByTestId('to-select');
+			const amountField = screen.getByTestId('amount');
+			const fromField = screen.getByTestId('from-select');
+			const toField = screen.getByTestId('to-select');
 
-		// 	userEvent.type(amountField, '100');
-		// 	userEvent.selectOptions(fromField, testObj.from);
-		// 	userEvent.selectOptions(toField, testObj.to);
+			userEvent.type(amountField, '100');
+			userEvent.selectOptions(fromField, testObj.from);
+			userEvent.selectOptions(toField, testObj.to);
 
-		// 	expect(action).toHaveBeenCalledTimes(1);
-		// 	expect(action).toHaveBeenCalledWith({
-		// 		amount: 100,
-		// 		from: 'PLN',
-		// 		to: 'USD',
-		// 	});
-		// 	// unmount component
-		// 	cleanup();
-		// }
+			expect(action).toHaveBeenCalledTimes(1);
+			expect(action).toHaveBeenCalledWith({
+				amount: 100,
+				from: 'PLN',
+				to: 'USD',
+			});
+			// unmount component
+			cleanup();
+		}
 	});
 });
